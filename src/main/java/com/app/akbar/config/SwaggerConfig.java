3db4b156ff2d9f79bfb33821ad67798b883f426b
akbar <akbarshaik9111@gmail.com>
2023-11-21 12:22:10 +0530

    Swagger and Connection Pooling Integration

---
+++ src/main/java/com/app/akbar/config/SwaggerConfig.java
@@ -0,0 +1,40 @@
+package com.app.akbar.config;
+/*
+import java.util.Collections;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+//@EnableSwagger2
+//@Configuration
+public class SwaggerConfig {
+	/*
+	@Bean
+	public Docket createDocket() {
+		return new Docket(DocumentationType.SWAGGER_2)
+				.select()
+				.apis(RequestHandlerSelectors.basePackage("com.app.akbar.rest"))
+				.paths(PathSelectors.any())
+				//.paths(PathSelectors.regex("/v1/api.*"))
+				.build()
+				.pathMapping("/")
+				.apiInfo(apiInfo());
+	}
+	
+	private ApiInfo apiInfo() {
+		return new ApiInfo("Spring Boot Crud", 
+				"CRUD Operations", 
+				"V1", 
+				"http://www.google.com", 
+				new Contact("AKBAR", "https://github.com/akbarshaik9111", "akbarshaik911@gmail.com"), 
+				"MAVEN", "https://mvnrepository.com/", Collections.emptyList());
+	}
+}*/
